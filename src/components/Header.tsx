0fdfae2a6e6894c9f83cf15081fe9579c3ae27d5
samenezes <61150892+samenezes@users.noreply.github.com>
2025-05-23 22:55:44 -0300

    Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="bg-blue-600 text-white py-6 shadow-md">
       <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold">Estude meu filho - Feito pela professora Sandra Alves </h1>
         <h1 className="text-3xl font-bold">Flutter Widgets Essenciais</h1>
         <p className="mt-2">Um guia completo para iniciantes com exemplos detalhados</p>
       </div>
